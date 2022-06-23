f015daa204f2cc755dbfe4466de3cd1420f1d031
Miguel Carlos Honorio <miguel.honorio@terramagna.com.br>
2022-06-23 17:46:24 -0300

    implement airtable repository

--- src/main.rs
+++ src/main.rs
@@ -1,4 +1,4 @@
-use repositories::pokemon::{InMemoryRepository, Repository, SqliteRepository};
+use repositories::pokemon::{AirtableRepository, InMemoryRepository, Repository, SqliteRepository};
 use std::sync::Arc;
 
 mod api;
@@ -11,7 +11,7 @@ extern crate serde;
 
 #[macro_use]
 extern crate clap;
-use clap::{App, Arg};
+use clap::{App, Arg, Values};
 
 fn main() {
     let matches = App::new(crate_name!())
@@ -19,9 +19,14 @@ fn main() {
         .author(crate_authors!())
         .arg(Arg::with_name("cli").long("cli").help("Runs in CLI mode"))
         .arg(Arg::with_name("sqlite").long("sqlite").value_name("PATH"))
+        .arg(
+            Arg::with_name("airtable")
+                .long("airtable")
+                .value_names(&["API_KEY", "WORKSPACE_ID"]),
+        )
         .get_matches();
 
-    let repo = build_repo(matches.value_of("sqlite"));
+    let repo = build_repo(matches.value_of("sqlite"), matches.values_of("airtable"));
 
     match matches.occurrences_of("cli") {
         0 => api::serve("localhost:8000", repo),
@@ -29,7 +34,16 @@ fn main() {
     }
 }
 
-fn build_repo(sqlite_value: Option<&str>) -> Arc<dyn Repository> {
+fn build_repo(sqlite_value: Option<&str>, airtable_values: Option<Values>) -> Arc<dyn Repository> {
+    if let Some(values) = airtable_values {
+        if let [api_key, workspace_id] = values.collect::<Vec<&str>>()[..] {
+            match AirtableRepository::try_new(api_key, workspace_id) {
+                Ok(repo) => return Arc::new(repo),
+                _ => panic!("Error while creating airtable repo"),
+            }
+        }
+    }
+
     if let Some(path) = sqlite_value {
         match SqliteRepository::try_new(path) {
             Ok(repo) => return Arc::new(repo),
